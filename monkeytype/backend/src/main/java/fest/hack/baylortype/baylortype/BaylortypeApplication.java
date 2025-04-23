4e10f06f790434f3dbd7b8a68624e5e0ab4ada2a
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2025-04-23 03:00:30 -0500

    init backend

---
+++ monkeytype/backend/src/main/java/fest/hack/baylortype/baylortype/BaylortypeApplication.java
@@ -0,0 +1,13 @@
+package fest.hack.baylortype.baylortype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaylortypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaylortypeApplication.class, args);
+	}
+
+}
---
+++ monkeytype/backend/src/test/java/fest/hack/baylortype/baylortype/BaylortypeApplicationTests.java
@@ -0,0 +1,13 @@
+package fest.hack.baylortype.baylortype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaylortypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
