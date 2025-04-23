d47e4403cf8cbe5e3dee97139ed739f701281cfc
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2025-04-23 04:16:48 -0500

    words generator

---
+++ monkeytype/backend/src/main/java/fest/hack/baylortype/service/WordService.java
@@ -0,0 +1,34 @@
+package fest.hack.baylortype.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class WordService {
+    private final Random random = new Random();
+
+    public String[] getAllWords() throws IOException {
+        return (Files.readString(Paths.get("words"))).split("\n");
+    }
+
+    public List<String> generateWords(int amount) throws IOException {
+        String[] words = getAllWords();
+        List<String> wordsList = new ArrayList<>();
+
+        for(int i = 0; i < amount; ++i){
+            wordsList.add(words[random.nextInt(words.length-3)]);
+        }
+
+        wordsList.add(words[words.length-3]);
+        wordsList.add(words[words.length-2]);
+        wordsList.add(words[words.length-1]);
+
+        return wordsList;
+    }
+}
