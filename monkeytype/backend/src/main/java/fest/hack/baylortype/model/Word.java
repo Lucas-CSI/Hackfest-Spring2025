92dec67213a609b4764664cb6c157d8852db6d78
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2025-06-05 14:38:40 -0500

    Create Word.java

---
+++ monkeytype/backend/src/main/java/fest/hack/baylortype/model/Word.java
@@ -0,0 +1,55 @@
+package fest.hack.baylortype.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "words")
+public class Word {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "word", nullable = false)
+    private String word;
+
+    @Column(name = "index", nullable = false)
+    private int wordIndex;
+
+    public WordEntity() {
+    }
+
+    public WordEntity(String word, int wordIndex) {
+        this.word = word;
+        this.wordIndex = wordIndex;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getWordIndex() {
+        return wordIndex;
+    }
+
+    public void setWordIndex(int wordIndex) {
+        this.wordIndex = wordIndex;
+    }
+}
